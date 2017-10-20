862e78106cc9c8cf684da54e127f914d80837a3b
AprilHall <april21abs@gmail.com>
2017-10-20 16:53:40 -0500

    excercise 3

--- maze.js
+++ maze.js
@@ -27,4 +27,31 @@ function changeBounds(){
 		for (var i = 0; i < bounds.length; i++) {
 		bounds[i].className = "boundary youlose";
 		}
- }
+}
+ 
+//excercise 3
+var bounds;
+var finish;
+
+window.onload = function() {
+ 
+     bounds = document.querySelectorAll(".boundary");
+	 finish = document.getElementById("end");
+	 finish.onmouseover = winner;
+	 
+     for (var x = 0; x < bounds.length; x++) {
+ 		bounds[x].onmouseover = loser;
+ 	}
+}
+
+function loser(){
+
+		for (var x = 0; x < bounds.length; x++) {
+			bounds[x].className = "boundary youlose";
+		}
+}
+		
+ 
+function winner(){
+		alert("You Win!");
+}
