88888c38273bde03f02104d401fe393f04144a22
AprilHall <april21abs@gmail.com>
2017-10-20 16:50:10 -0500

    excercise 2

--- maze.js
+++ maze.js
@@ -10,3 +10,21 @@ var bound = document.getElementById("boundary1");
 	}
 	
 }
+
+//excercise 2
+var bounds;
+
+window.onload = function() {
+
+		bounds = document.querySelectorAll(".boundary");
+		for (var i = 0; i < bounds.length; i++) {
+		bounds[i].onmouseover = changeBounds;
+		}
+}
+ 
+function changeBounds(){
+
+		for (var i = 0; i < bounds.length; i++) {
+		bounds[i].className = "boundary youlose";
+		}
+ }
