8efaf4fe8aa55097426560bf1e0d587148e65a74
AprilHall <april21abs@gmail.com>
2017-10-20 16:38:29 -0500

    excercise 1

---
+++ maze.js
@@ -0,0 +1,12 @@
+//excercise 1
+window.onload = function() {
+
+var bound = document.getElementById("boundary1");
+
+	bound.onmouseover = function(){
+		bound.className = "boundary youlose";
+		
+		
+	}
+	
+}
