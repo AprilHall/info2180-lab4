6e082906637745554fbd8bac231902dc4f5b6d18
AprilHall <april21abs@gmail.com>
2017-10-20 18:09:51 -0500

    excercise 4

--- maze.js
+++ maze.js
@@ -55,3 +55,109 @@ function loser(){
 function winner(){
 		alert("You Win!");
 }
+
+//execercise 4
+var bounds;
+var begin;
+var finish;
+
+window.onload = function() {
+ 
+     bounds = document.querySelectorAll(".boundary");
+	 finish = document.getElementById("end");
+	 finish.onmouseover = winner;
+	 
+	 begin = document.getElementById("start");
+     begin.onclick = restart;
+	 
+     for (var x = 0; x < bounds.length; x++) {
+ 		bounds[x].onmouseover = loser;
+ 	}
+}
+
+function loser(){
+
+		for (var x = 0; x < bounds.length; x++) {
+			bounds[x].className = "boundary youlose";
+		}
+}
+		
+ 
+function winner(){
+		alert("You Win!");
+}
+
+function restart(){
+	for (var x = 0; x < bounds.length; x++) {
+		bounds[x].className = "boundary";
+	}
+}
+
+/*
+//excercise 5
+var bounds;
+var begin;
+var result;
+var finish;
+var limit = false;
+var complete = false;
+
+
+window.onload = function() {
+
+		bounds = document.querySelectorAll(".boundary");
+		begin = document.getElementById("start");
+		result = document.getElementById("status");
+		finish = document.getElementById("end");
+
+		begin.onclick = restart;
+		
+		finish.onmouseover = winner;
+		
+		for (var x = 0; x < bounds.length; x++) { 
+			bounds[x].onmouseover = loser;
+		}
+}
+
+function loser(){ 
+
+	if (!complete){ 
+		if(!limit)
+		{
+			limit = true;
+			
+			result.innerText = 'Sorry, you lose! Click on "S" to restart game.';
+			
+			for (var x = 0; x < bounds.length; x++) {
+				bounds[x].className = "boundary youlose";
+			}
+		}
+	}
+}
+
+function winner(){
+	
+		if(!limit){ 
+			complete = true;
+			
+			result.innerText = 'Yay, you won! Click on "S" to restart game.';
+		}
+
+}
+
+function restart(){
+	
+	    limit = false;
+		
+		complete = false;
+		
+		result.innerText = 'Move your mouse over the "S" to start game.';	
+		
+		for (var x = 0; x < bounds.length; x++) {
+			bounds[x].className = "boundary";
+	}
+}
+
+
+//excercise 6
+*/
